7b42cc1b1ceb28151f97f74f18f46833e8ea329c
Marianna <Marianna.l.americo@gmail.com>
2023-03-28 16:42:33 -0300

    Batista & Moraes questão 2

---
+++ BeMQuestao2.java
@@ -0,0 +1,21 @@
+public class BeMQuestao2 {
+    public static void main(String[] args){
+         int i = 1;
+        System.out.println("i = " + i);
+
+        long j = 2L;
+        System.out.println("j = " + j);
+
+        float p = 20.0f;
+        System.out.println("p = " + p);
+
+        double q = 30.0;
+        System.out.println("q = " + q);
+
+        boolean b = true;
+        System.out.println("b = " + b);
+
+        char c = 'k';
+        System.out.println("c = " + c);
+    }
+}
