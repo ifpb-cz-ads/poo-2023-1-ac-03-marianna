b948f640b424262fdf1e44091e1f2f2d0ab1ffb1
Marianna <Marianna.l.americo@gmail.com>
2023-03-28 16:11:44 -0300

    Atividade prática 4

---
+++ Atividade4.java
@@ -0,0 +1,6 @@
+public class Atividade4 {
+    public static void main(String[] args){
+        int metroCub = 40;
+        System.out.println("O valor total de contrucao e: R$" +metroCub*100);
+    }
+}
