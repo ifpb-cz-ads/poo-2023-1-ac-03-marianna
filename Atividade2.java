3d14be15f3eeb0ef836a2a97af1ce0407be3533a
Marianna <Marianna.l.americo@gmail.com>
2023-03-28 16:06:29 -0300

    Atividade prática 2

---
+++ Atividade2.java
@@ -0,0 +1,9 @@
+public class Atividade2{
+public static void main(String[] args) {
+    int valor1 = 5;
+    int valor2 = 9;
+    int valor3 = 7;
+    System.out.println("Resultado da soma: " +(valor1+valor2+valor3));
+    System.out.println("Resultado da media: " +(valor1+valor2+valor3) / 3);
+    }
+}
